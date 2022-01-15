0fab6c7710f2fd918ab0504703361a34f2a484f3
Hung Pham <hungphamvn.plh@gmail.com>
2022-01-15 15:48:41 +0700

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 ssh -o StrictHostKeyChecking=no $SSH_USER@$DEV_HOST << 'ENDSSH'
   set -e
-  cd /home/chonagiau/ch-nha-giau/
+  cd /home/chonhagiau/ch-nha-giau/
   git pull origin dev
   exec "$@"
 ENDSSH
