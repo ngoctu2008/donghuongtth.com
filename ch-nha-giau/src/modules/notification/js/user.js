ebfac0c45f813ebccdcc1d9d563872e839034c0f
Hoàng Nguyễn <hoangnguyen@chonhagiau.com>
2022-01-20 09:38:40 +0700

    rebuild code
    Signed-off-by: Hoàng Nguyễn <hoangnguyen@chonhagiau.com>

--- ch-nha-giau/src/modules/notification/js/user.js
+++
@@ -1,7 +0,0 @@
-/**
- * @Project NUKEVIET 4.x
- * @Author VINADES.,JSC (contact@vinades.vn)
- * @Copyright (C) 2015 VINADES.,JSC. All rights reserved
- * @License GNU/GPL version 2 or any later version
- * @Createdate Wed, 06 May 2015 02:22:19 GMT
- */
--- ch-nha-giau/src/themes/default/modules/slider/block.slider_left.tpl
+++
@@ -1,8 +0,0 @@
-<!-- BEGIN: main -->
-
-	<!-- BEGIN: loop -->
-	<a href="{DATA.link}" title="{DATA.title}"> <img src="{DATA.image}" class="banner_home_left" alt="{DATA.title}"></a>
-	<!-- END: loop -->
-
-<!-- END: main -->
-
