37db1ef2901937bfcca97dc9f7db288414a6009d
Hoàng Nguyễn <hoangnguyen@chonhagiau.com>
2022-01-21 21:24:58 +0700

    test info order
    Signed-off-by: Hoàng Nguyễn <hoangnguyen@chonhagiau.com>

--- src/modules/retails/funcs/payment.php
+++ src/modules/retails/funcs/payment.php
@@ -214,6 +214,7 @@ if ($thanhtoan)
 		$inputData['format_vnp_Amount'] = number_format($inputData['vnp_Amount']/100,0,",",",");
 		$xtpl->assign('thanhtoan', $inputData);
 	}elseif($payment_method == 'recieve'){
+		print_r($info_order);
 		$inputData['vnp_txnref'] = implode(' - ',$array_order);
 		
 		$inputData['date_create'] = date("d/m/Y H:i",$info_order['time_add']);//$ngay . '/' . $thang . '/' . $nam . ' - ' . $gio . ':' . $phut;
