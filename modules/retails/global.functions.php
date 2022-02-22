d4f19a5f0ec38af5d4dc6c96264e5a963ad6f286
Hoàng Nguyễn <hoangnguyen@chonhagiau.com>
2022-02-22 14:03:33 +0700

    add payment_method for refund
    Signed-off-by: Hoàng Nguyễn <hoangnguyen@chonhagiau.com>

--- modules/retails/global.functions.php
+++ modules/retails/global.functions.php
@@ -4493,9 +4493,10 @@ function momo_refund($info_order)
 		// lưu thông tin lịch sử hoàn tiền vnpay
 		//$row['responseTime'] = NV_CURRENTTIME;
 		$responsecode = ($data['resultCode'] == 0) ? '0' : $data['resultCode'];
-		$stmt = $db->prepare('INSERT INTO ' . TABLE . '_payment_refund (order_id, responsecode, message, user_add, time_add) VALUES (:order_id, :responsecode, :message, :user_add, :time_add)');
+		$stmt = $db->prepare('INSERT INTO ' . TABLE . '_payment_refund (order_id, payment_method, responsecode, message, user_add, time_add) VALUES (:order_id, :payment_method, :responsecode, :message, :user_add, :time_add)');
 
 		$stmt->bindParam(':time_add', $data['responseTime'], PDO::PARAM_INT);
+		$stmt->bindParam(':payment_method', $info_order['payment_method'], PDO::PARAM_STR);
 		$stmt->bindParam(':order_id', $info_order['id'], PDO::PARAM_INT);
 		$stmt->bindParam(':responsecode', $responsecode, PDO::PARAM_STR);
 		$stmt->bindParam(':message', $data['message'], PDO::PARAM_STR);
